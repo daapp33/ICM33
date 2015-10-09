fa6cac6c4c6eb9063f5ee623a490c74e0161f904
Sogapp <damien.fourcade@gmail.com>
2015-10-10 00:19:33 +0200

    Material OK

--- www/js/app.js
+++ www/js/app.js
@@ -8,31 +8,31 @@
 angular.module('icoming', ['ionic', 'icoming.controllers', 'icoming.services'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleLightContent();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicMaterialConfigProvider) {
+    $ionicMaterialConfigProvider.enableForAllPlatforms();
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            cache: false,
+            templateUrl: "templates/home.html",
+            controller: "HomeCtrl"
+        })
 
-  $stateProvider
-    .state('home', {
-    url: '/home',
-    cache: false,
-    templateUrl: "templates/home.html",
-    controller: "HomeCtrl"
-  })
-
-  $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,11 @@
 angular.module('icoming.controllers', [])
 
-.controller('HomeCtrl', function($scope,$ionicPopup) {
-  /*TODO*/
-  $scope.test=function(){
-    $ionicPopup.alert({
-                title: 'Anthooooo???',
-                okType: 'button-balanced'
-            });
-  }
+.controller('HomeCtrl', function($scope, $ionicPopup) {
+    /*TODO*/
+    $scope.test = function() {
+        $ionicPopup.alert({
+            title: 'Anthooooo???',
+            okType: 'button-balanced'
+        });
+    }
 });
