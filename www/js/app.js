4cab716072299305ef6dcdae507e7c23d33841cc
Sogapp <damien.fourcade@gmail.com>
2015-10-10 15:12:31 +0200

    Autocompletion adresse

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 
-angular.module('icoming', ['ionic', 'icoming.controllers', 'icoming.services'])
+angular.module('icoming', ['ionic', 'icoming.controllers', 'icoming.services', 'google.places'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
