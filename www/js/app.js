43eeca8f8fdc88808f7de43a6d26b3cedb51dec6
Sogapp <damien.fourcade@gmail.com>
2015-10-22 21:55:46 +0200

    test contact

--- www/js/app.js
+++ www/js/app.js
@@ -77,11 +77,17 @@ angular.module('icoming', ['ionic', 'icoming.controllers', 'icoming.services', '
             };
             onSuccess(contact);
         } else {
+            navigator.contacts.pickContact(function(contact) {
+                console.log('The following contact has been selected:' + JSON.stringify(contact));
+            }, function(err) {
+                console.log('Error: ' + err);
+            });
+
             // find all contacts with 'Bob' in any name field
             var options = new ContactFindOptions();
-            //options.filter = "Bob";
+            options.filter = "Aaum";
             options.multiple = true;
-            options.desiredFields = [navigator.contacts.fieldType.id];
+            //options.desiredFields = [navigator.contacts.fieldType.id];
             options.hasPhoneNumber = true;
             var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
             navigator.contacts.find(fields, onSuccess, onError, options);
